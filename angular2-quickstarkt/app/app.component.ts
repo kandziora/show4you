0b0d51be813217fc0ab039d07a6a365a964806e5
Jörg Kandziora <s55814@beuth-hochschule.de>
2016-05-17 10:06:18 +0200

    theheroEditor

---
+++ angular2-quickstarkt/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Show4 You</h1>',
+    template: '<h2>Peter</h2>'
+})
+export class AppComponent { }
