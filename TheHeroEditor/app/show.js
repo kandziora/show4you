7b9d245e43fb0e41d36f6294b663e8bc2e969ca0
joerg <s55814@beuth-hochschule.de>
2016-05-24 15:45:36 +0200

    new Branch added.

--- TheHeroEditor/app/show.js
+++ TheHeroEditor/app/show.js
@@ -1,17 +1,8 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var Show;
-    return {
-        setters:[],
-        execute: function() {
-            Show = (function () {
-                function Show() {
-                }
-                return Show;
-            }());
-            exports_1("Show", Show);
-        }
+"use strict";
+var Show = (function () {
+    function Show() {
     }
-});
+    return Show;
+}());
+exports.Show = Show;
 //# sourceMappingURL=show.js.map
