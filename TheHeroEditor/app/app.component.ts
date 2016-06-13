db306ca20d09c78a3780beb807f2acf087656a41
Jorge Ayala <gorge.ayala@gmail.com>
2016-06-13 13:21:56 +0200

    Project npm package updated.

--- TheHeroEditor/app/app.component.ts
+++ TheHeroEditor/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 import { ShowsDetailComponent } from './shows-detail.component';
 import { Show } from './show';
 
--- TheHeroEditor/app/shows-detail.component.ts
+++ TheHeroEditor/app/shows-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from 'angular2/core';
+import { Component, Input } from '@angular/core';
 import { Show } from './show';
 
 
@@ -6,12 +6,20 @@ import { Show } from './show';
 @Component({
     selector: 'my-show-detail',
     template: `
-        <div>
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
+        <div *ngIf="selectedShow">
+            <h2>{{selectedShow.name}} details!</h2>
+            <div><label>id: </label>{{selectedShow.id}}</div>
             <div>
                 <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+                <input [(ngModel)]="selectedShow.name" placeholder="name"/>
+            </div>
+            <div>
+                <label>genre: </label>
+                <input [(ngModel)]="selectedShow.genre" placeholder="genre"/>
+            </div>
+            <div>
+                <label>support: </label>
+                <input [(ngModel)]="selectedShow.support" placeholder="support"/>
             </div>
         </div>
 `
